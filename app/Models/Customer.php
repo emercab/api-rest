49e0014cb1492952378ee010736d6f7644285396
Emerson Cabrera <info@emersoncabrera.com>
2024-02-25 21:24:30 -0500

    First commit

---
+++ app/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Customer extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [];
+
+  /**
+   * Get all of the invoices for the Customer
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasMany
+   */
+  public function rInvoices(): HasMany
+  {
+    return $this->hasMany(Invoice::class);
+  }
+}
