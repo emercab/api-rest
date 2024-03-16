0d161cefd26abba988ddc213994f03e9f4433b44
Emerson Cabrera <info@emersoncabrera.com>
2024-03-15 21:05:24 -0500

    PUT & PATCH methods have been completed.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -79,6 +79,7 @@ class CustomerController extends Controller
   public function update(UpdateCustomerRequest $request, Customer $customer)
   {
     //
+    $customer->update( $request->all() );
   }
 
   /**
